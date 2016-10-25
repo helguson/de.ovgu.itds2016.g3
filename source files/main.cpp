17512cfb294eb55fe2c518f776f6d9d46309b999
Thomas Hennig <hevidera@gmail.com>
2016-10-25 11:28:38 +0200

    removed unused code

--- source files/main.cpp
+++ source files/main.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <string>
-
 #include "Controller.h"
-
-void log(std::string message) {
-	std::cout << message << std::endl;
-}
 
 int main() {
 	Controller controller;
