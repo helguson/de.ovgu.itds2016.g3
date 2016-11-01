6004ac75f9aa438757e2844f73218d4ed3b4bc3f
Thomas Hennig <hevidera@gmail.com>
2016-11-01 14:52:57 +0100

    initial tree wrapper files

---
+++ source files/ThreeDTree.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class ThreeDTree {
+public:
+	// std::vector<Point3D> query(const ? range) const;
+	// std::vector<Point3D> query(const Point3d point, const double distance) const;
+	// Point3D estimateNearNeighborOf(const Point3d point) const;
+
+private:
+	// std::shared_ptr<Node> _root;
+};
