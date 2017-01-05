7818461eceaad41537df2429ca851a286ffda2c5
Thomas Hennig <hevidera@gmail.com>
2017-01-05 10:17:55 +0100

    removed line command
    - no need

--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -1,21 +1,7 @@
 #include "Controller.h"
 
-#include "LineCommand.h"
-
 int main(int argc, char *argv[])
 {
-
-	std::string s1 = "hello";
-	std::string s2 = decodeFrom<std::string>(s2);
-
-	int i = decodeFrom<int>(s1);
-	double d = decodeFrom<double>(s1);
-
-	process<>();
-	//process<int, int>(s1, s1);
-	//process<std::string, int, float>(s1, s1, s1);
-
-
 	Controller controller(argc, argv);
 	return controller.startApplication();
 }
